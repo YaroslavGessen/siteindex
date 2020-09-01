bde71751d5d9eed6b60acc16c7f7d78bd551ab6d
Yaroslav <44481165+YaroslavGessen@users.noreply.github.com>
2020-09-01 12:56:32 +0300

    Update ElasticsearchUtil.java

--- src/main/java/com/yaroslav/siteindex/util/ElasticsearchUtil.java
+++ src/main/java/com/yaroslav/siteindex/util/ElasticsearchUtil.java
@@ -1,5 +1,6 @@
 package com.yaroslav.siteindex.util;
 
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yaroslav.siteindex.json.UrlSearchDoc;
 import com.squareup.okhttp.*;
@@ -12,11 +13,15 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+
 @Component
 public class ElasticsearchUtil {
 
     @Value("${elastic.base.url}")
     private String baseUrl;
+
+    private static final String API_KEY = "site:4610c374b4b7ae2bd80f0a8d1384a12f";
 
     @Autowired
     RestTemplate restTemplate;
@@ -28,7 +33,7 @@ public class ElasticsearchUtil {
 
     public  void addData(UrlSearchDoc doc)  {
         try {
-            String auth =  new String(Base64.encodeBase64("site:8c6d4815e5340e273775354f46e86774".getBytes()));
+            String auth =  new String(Base64.encodeBase64(API_KEY.getBytes()));
             String content = om.writeValueAsString(doc);
             OkHttpClient client =  new OkHttpClient();
             MediaType mediaType = MediaType.parse("application/json");
@@ -48,37 +53,52 @@ public class ElasticsearchUtil {
         }
     }
 
-    public  Response search()  {
-        Response response = null;
-        try {
-            OkHttpClient client = new OkHttpClient();
-            Request request = new Request.Builder()
-                    .url("https://site:8c6d4815e5340e273775354f46e86774@gimli-eu-west-1.searchly.com/elastic/_search" )
-                    .method("GET", null)
-                    .build();
-            response = client.newCall(request).execute();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return response;
+    public String searchEverything(String text) throws IOException {
+        String auth =  new String(Base64.encodeBase64(API_KEY.getBytes()));
+        OkHttpClient client = new OkHttpClient();
+        MediaType mediaType = MediaType.parse("application/json");
+        Request request = new Request.Builder()
+                .url(baseUrl + "/_search?q=" + text)
+                .addHeader("Content-Type", "application/json")
+                .addHeader(HttpHeaders.AUTHORIZATION, "Basic " + auth)
+                .build();
+        Response response = client.newCall(request).execute();
+        String res = response.body().string();
+        return res;
     }
-    public String getData(String keyword) {
-        try {
-            String auth = new String(Base64.encodeBase64("site:4610c374b4b7ae2bd80f0a8d1384a12f".getBytes()));
-            OkHttpClient client = new OkHttpClient();
-            MediaType mediaType = MediaType.parse("application/json");
-            Request request = new Request.Builder()
-                    .url(baseUrl + "/_search?q=" + keyword)
-                    .method("GET", null)
-                    .addHeader("Content-Type", "application/json")
-                    .addHeader(HttpHeaders.AUTHORIZATION, "Basic " + auth)
-                    .build();
-            Response res = client.newCall(request).execute();
-            return res.body().string();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "";
+
+
+    public String search(String crawlId, String text) throws IOException {
+        String auth =  new String(Base64.encodeBase64(API_KEY.getBytes()));
+        OkHttpClient client = new OkHttpClient();
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, buildBody(crawlId, text));
+        Request request = new Request.Builder()
+                .url(baseUrl + "/_search")
+                .method("POST", body)
+                .addHeader("Content-Type", "application/json")
+                .addHeader(HttpHeaders.AUTHORIZATION, "Basic " + auth)
+                .build();
+        Response response = client.newCall(request).execute();
+        String res = response.body().string();
+
+        return res;
+    }
+
+    private String buildBody(String crawlId, String text){
+        return " {\r\n" +
+                "  \"query\": {\r\n" +
+                "    \"bool\": {\r\n" +
+                "      \"must\": {\r\n" +
+                "        \"bool\" : {\r\n" +
+                "          \"must\": [\r\n" +
+                "            { \"match\": { \"crawlId\": \"" + crawlId + "\" }} ,\r\n" +
+                "            { \"match\": { \"content\": \"" + text + "\"}} \r\n" +
+                "           ]\r\n" +
+                "        }\r\n" +
+                "      }\r\n" +
+                "    }\r\n" +
+                "  }\r\n" +
+                "}";
     }
 }
